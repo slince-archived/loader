12295469d4540a20e19362b12fc76a567c4be259
slince <taosikai@sina.cn>
2014-09-16 18:21:52 +0800

    ss

--- src/Loader/ClassMapLoader.php
+++ src/Loader/ClassMapLoader.php
@@ -1,1 +1,29 @@
 <?php
+/**
+ * slince cache library
+ * @author Taosikai <taosikai@yeah.net>
+ */
+namespace Slince\Loader\Loader;
+
+use Slince\Loader\LoaderInterface;
+
+class ClassMapLoader implements LoaderInterface
+{
+    private $_classMap = [];
+    
+    function setClassMapping($class, $file)
+    {
+        $this->_classMap[$class] = $file;
+    }
+    
+    function loadClass($class)
+    {
+        $file = $this->findFile($class);
+        include $file;
+    }
+    
+    function findFile($class)
+    {
+        
+    }
+} 
--- src/LoaderInterface.php
+++ src/LoaderInterface.php
@@ -7,5 +7,13 @@ namespace Slince\Loader;
 
 interface LoaderInterface
 {
-    
+    /**
+     * 加载类
+     */ 
+    function loadClass($class);
+    /**
+     * 查找类文件
+     * @param string $class
+     */
+    function findFile($class);
 }
