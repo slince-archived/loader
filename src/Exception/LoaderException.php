4b013bef92c03fc1970e01c15fe888b8c9705ac4
slince <taosikai@sina.cn>
2014-09-17 13:38:23 +0800

    add

---
+++ src/Exception/LoaderException.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * slince cache library
+ * @author Taosikai <taosikai@yeah.net>
+ */
+namespace Slince\Loader\Exception;
+
+class LoaderException extends \Exception
+{
+}
---
+++ src/Factory.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * slince cache component
+ * @author Taosikai <taosikai@yeah.net>
+ */
+namespace Slince\Loader;
+
+use Slince\Loader\Loader;
+
+class Factory
+{
+
+    function create($loaderType)
+    {
+        switch ($loaderType) {
+            
+        }
+    }
+}
